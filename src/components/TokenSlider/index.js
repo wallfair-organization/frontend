bfae4cfc5d2d77c9514412b6205bacae624a42d8
Chuong Tran <chuong14011991@gmail.com>
2021-09-18 15:46:00 +0700

    Update slider
    To change from raw value to percentage

--- src/components/TokenSlider/index.js
+++ src/components/TokenSlider/index.js
@@ -1,6 +1,7 @@
 import { withStyles } from '@material-ui/core/styles';
 import Slider from '@material-ui/core/Slider';
 import styles from './styles.module.scss';
+import { useEffect, useState } from 'react';
 
 const TSlider = withStyles({
   root: {
@@ -38,56 +39,120 @@ const TSlider = withStyles({
     borderRadius: 3,
   },
   rail: {
-    height: 6,
+    height: 4,
+    marginTop: 1,
     borderRadius: 3,
     color: '#6751EC',
     opacity: 1,
   },
   mark: {
-    width: 0,
-    height: 0,
+    // width: '14px',
+    // height: '14px',
+    // border: '2px solid #fff',
+    // borderRadius: '50%',
+    // backgroundColor: '#6751ec',
+    // top: '50%',
+    // transform: 'translateY(-50%)',
+    // '&:nth-last-of-type(1)': {
+    //   display: 'none !important'
+    // },
+    display: 'none',
   },
   markLabel: {
-    top: -5,
-    fontFamily: 'PlusJakarta-Regular',
-    fontSize: 11,
-    fontStyle: 'normal',
-    fontWeight: 400,
-    letterSpacing: 0,
-    textAlign: 'left',
-    marginLeft: -1,
+    top: '50%',
+    transform: 'translateY(-50%)',
+    '& > span': {
+      display: 'block',
+      width: '15px',
+      height: '15px',
+      border: '2px solid #fff',
+      borderRadius: '50%',
+      backgroundColor: '#6751ec',
+      '& > span': {
+        display: 'block',
+        transform: 'translateY(-100%)',
+      },
+    },
+    '& .mark--small': {
+      width: '12px',
+      height: '12px',
+    },
+    '& .mark--last': {
+      right: 0,
+      left: 'unset',
+      transform: 'translateX(-50%)',
+
+      '& > span': {
+        display: 'block',
+        position: 'absolute',
+        right: 0,
+      },
+    },
+    // top: -5,
+    // fontFamily: 'PlusJakarta-Regular',
+    // fontSize: 11,
+    // fontStyle: 'normal',
+    // fontWeight: 400,
+    // letterSpacing: 0,
+    // textAlign: 'left',
+    // marginLeft: -1,
   },
 })(Slider);
 
-const TokenSlider = ({ value, setValue, maxValue, ...props }) => {
+const TokenSlider = ({ maxValue, setValue, ...props }) => {
+  const [sliderValue, setSliderValue] = useState(1);
+  const minValue = 0;
   if (maxValue < 1) {
     maxValue = 2800;
   }
 
-  const marks = [
-    {
-      value: 1,
-      label: 1,
-    },
-    {
-      value: maxValue,
-      label: maxValue,
-    },
-  ];
+  useEffect(() => {
+    setValue((sliderValue / 100) * (maxValue - minValue));
+  }, [sliderValue]);
+
+  // const marks = [
+  //   {
+  //     value: 1,
+  //     label: 1,
+  //   },
+  //   {
+  //     value: maxValue,
+  //     label: maxValue,
+  //   },
+  // ];
+  const points = [0, 25, 50, 75, 100];
+  const marks = points.map((point, pointIndex) => ({
+    value: point,
+    label: (
+      <span
+        className={`${[25, 75].includes(point) ? 'mark--small' : ''} 
+          ${pointIndex === points.length - 1 ? 'mark--last' : ''}
+        `}
+      >
+        <span>{[0, 100].includes(point) && `${point}%`}</span>
+      </span>
+    ),
+  }));
 
   return (
     <TSlider
       valueLabelDisplay="auto"
       aria-label="pretto slider"
       min={1}
-      max={maxValue}
-      value={value}
+      max={100}
+      value={sliderValue}
       onChange={(event, v) => {
-        setValue(v);
+        setSliderValue(v);
       }}
       valueLabelDisplay="on"
       className={styles.tokenSlider}
       marks={marks}
+      step={1}
+      components={{
+        Mark: 'div',
+      }}
+      getAriaLabel={() => 'getAriaLabel'}
+      valueLabelFormat={value => `${value}%`}
     />
   );
 };
