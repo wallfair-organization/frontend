c0677f8645ec18f574aba2553e44e7280eaebbc5
Suryakant Gupta <suryakantgupta66@gmail.com>
2021-09-18 16:39:53 +0530

    Ofline badge created and integrated

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './styles.module.scss';
 import LiveBadge from '../LiveBadge';
+import OfflineBadge from '../OfflineBadge';
 import ViewerBadge from '../ViewerBadge';
 import Tags from '../Tags';
 import TimeLeftCounter from '../TimeLeftCounter';
@@ -31,9 +32,14 @@ const EventCard = ({
         ></div>
         <div className={styles.eventCardBackground}></div>
         <div>
-          {live && (
+          {live ? (
             <>
               <LiveBadge />
+              <ViewerBadge viewers={viewers} />
+            </>
+          ) : (
+            <>
+              <OfflineBadge />
               <ViewerBadge viewers={viewers} />
             </>
           )}
